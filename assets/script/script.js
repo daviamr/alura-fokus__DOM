edcbfef94bb9f9e38f76c60eba4b5d358e45ac96
Davi (Estudo) <daviamorim_sz@live.com>
2023-08-28 01:23:02 -0300

    fixs and feats

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,6 +8,20 @@ const longB = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const tittle = document.querySelector(".app__title");
 
+const music = new Audio("assets/sound/luna-rise-part-one.mp3");
+music.loop = true;
+const audioPlaySound = new Audio("assets/sound/play.wav");
+const audioPauseSound = new Audio("assets/sound/pause.mp3");
+const audioBeepSound = new Audio("assets/sound/beep.mp3");
+const musicButton = document.querySelector("#alternar-musica");
+
+const timer = document.querySelector("#timer");
+const startTimerButton = document.querySelector("#start-pause");
+let elapsedTInSeconds = 5;
+let intervaloId = null;
+
+const startPauseText = document.querySelector("#start-pause-text");
+
 focusB.addEventListener("click", () => {
   alterarContexto("foco");
   focusB.classList.add("active");
@@ -52,4 +66,61 @@ function alterarContexto(contexto) {
       console.log("Nenhum texto encontrado.");
       break;
   }
+}
+
+// Music
+
+musicButton.addEventListener("change", () => {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+    music.currentTime = 0;
+  }
+});
+
+// Temporizador
+const countdown = () => {
+  if (elapsedTInSeconds <= 0) {
+    zerarContagem();
+    alteraTexto("Começar");
+    alert("Tempo de descanso esgotado, volte a estudar ;)");
+    audioBeepSound.play();
+    return;
+  }
+  elapsedTInSeconds -= 1;
+  console.log("Temporizador:" + elapsedTInSeconds);
+};
+
+// Evento de click no botão de música
+startTimerButton.addEventListener("click", () => {
+  iniciarOuPausarContagem();
+  if (startPauseText.textContent == "Começar") {
+    alteraTexto("Pausar");
+  } else {
+    alteraTexto("Começar");
+  }
+});
+
+// Func Inciar e Pausar contagem
+function iniciarOuPausarContagem() {
+  if (intervaloId) {
+    zerarContagem();
+    audioPauseSound.play();
+    return;
+  }
+
+  audioPlaySound.play();
+  intervaloId = setInterval(countdown, 1000);
+}
+
+// Func Zerar Contagem
+function zerarContagem() {
+  clearInterval(intervaloId);
+  intervaloId = null;
+}
+
+// Func Alterar texto do button
+function alteraTexto(parametro) {
+  startPauseText.textContent = `${parametro}`;
 }
